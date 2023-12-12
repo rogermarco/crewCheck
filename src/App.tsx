0d1c798e77ec5a984cf86e0a230131118079410f
rogermarco <marcrogers90@gmail.com>
2023-12-12 18:28:11 +0100

    initial tailwind config and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
+import clapper from './assets/clapper.svg'
+
 function App() {
   return (
-    <main className="w-4/5 m-auto">
+    <main className='w-[90%] m-auto'>
       <section>
-        <p>
-          Drop one imdb <strong>nm parameter</strong> from a link (ex:
-          imdb/name/<strong>nm1234567</strong>) in each box to compare the two. 
+        <p className='text-center mt-5'>
+          Drop a full imdb link in each box to compare the two to see if they have worked together before.
         </p>
-        <p>Matches will be shown below.</p>
+        <p className='text-center'>Hit the clapper and any matches will be shown below!</p>
         <form>
-          <input
-            type='text'
-            placeholder='First person'
-            id='nameOne'
-            name='nameOne'
-          ></input>
-          <input
-            type='text'
-            placeholder='Second person'
-            id='nameTwo'
-            name='nameTwo'
-          ></input>
-          <button type='submit'>Compare!</button>
+          <div className='flex flex-col sm:flex-row'>
+            <input
+              type='text'
+              placeholder='First person'
+              id='nameOne'
+              name='nameOne'
+              className='input-box'
+            ></input>
+            <input
+              type='text'
+              placeholder='Second person'
+              id='nameTwo'
+              name='nameTwo'
+              className='input-box'
+            ></input>
+          </div>
+          <img 
+          src={clapper}
+          className='transition ease-in-out hover:scale-105 h-60 w-auto m-auto mt-5'
+          />
         </form>
       </section>
     </main>
