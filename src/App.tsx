98833e60bd3d46d8eea10c0801b7a68a909660d7
rogermarco <marcrogers90@gmail.com>
2023-12-14 19:38:02 +0100

    initial full compare and results rendering

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ function App() {
     callTwo: {},
   });
 
+  const [matches, setMatches] = useState<[]>([]);
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormState((prevState) => ({
@@ -22,14 +24,28 @@ function App() {
     }));
   };
 
+  const comparer = async (one, two) => {
+    const idArrayOne = one.filmography;
+    const idArrayTwo = two.filmography;
+    
+    const badCategories = ['archive_footage', 'self', 'thanks', 'soundtrack']
+    const idMatches = await idArrayTwo.filter(({ id }) => idArrayOne.map(entry => entry.id).includes(id)).filter(({ category }) => !badCategories.includes(category)).map(entry => entry.id);
+    console.log('IDMATCHES', idMatches);
+
+    const filmMatches = idArrayOne.filter(film => idMatches.includes(film.id));
+    
+    // setMatches();
+  };
+
   const handleClick = async () => {
     try {
       const trimmedNameOne = formState.nameOne.match(/nm\d*/g)!.join();
       const trimmedNameTwo = formState.nameTwo.match(/nm\d*/g)!.join();
       const resOne = await apiCall(trimmedNameOne);
       const resTwo = await apiCall(trimmedNameTwo);
+      comparer(resOne, resTwo);
 
-      setResultState({callOne: resOne, callTwo: resTwo})
+      setResultState({ callOne: resOne, callTwo: resTwo });
     } catch (error) {
       console.log(error);
       alert('something went wrong with the form submission');
@@ -68,17 +84,16 @@ function App() {
           </div>
           <img
             src={clapper}
-            className='transition ease-in-out hover:scale-105 h-auto w-2/5 m-auto mt-5 cursor-pointer'
+            className='transition ease-in-out hover:scale-105 h-auto w-2/5 m-auto my-5 cursor-pointer'
             onClick={handleClick}
           />
         </form>
       </section>
-      {resultState.callOne.id && resultState.callTwo.id &&
+      {resultState.callOne.id && resultState.callTwo.id && (
         <section>
-          <Results result={resultState.callOne} />
-          <Results result={resultState.callTwo} />
+          <Results resultOne={resultState.callOne} resultTwo={resultState.callTwo} matches={matches} />
         </section>
-      }
+      )}
     </main>
   );
 }
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,51 +1,92 @@
-import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from 'uuid'; 
+import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-function Results(result: any) {
-  const [categoryList, setCategoryList] = useState({})
-  console.log(result);
+function Results({ resultOne, resultTwo, matches }) {
+
+  const idArrayOne = resultOne.filmography;
+  const idArrayTwo = resultTwo.filmography;
   
-  useEffect(() => {
-    const filmArray = result.result.filmography;
-    const list = {};
-    for (const index of filmArray) {
-      list[index.category] = list[index.category] ? list[index.category] + 1 : 1;
+  const badCategories = ['archive_footage', 'self', 'thanks', 'soundtrack']
+  const idMatches = idArrayTwo.filter(({ id }) => idArrayOne.map(entry => entry.id).includes(id)).filter(({ category }) => !badCategories.includes(category)).map(entry => entry.id);
+  // console.log('IDMATCHES', idMatches);
+
+  const filmOneMatches = idArrayOne.filter(film => idMatches.includes(film.id)).filter(({ category }) => !badCategories.includes(category));
+  const filmTwoMatches = idArrayTwo.filter(film => idMatches.includes(film.id)).filter(({ category }) => !badCategories.includes(category));
+  // console.log('FILM ONE MATCHES', filmOneMatches);
+  // console.log('FILM TWO MATCHES', filmTwoMatches);
+
+  // console.log(filmOneMatches[0].id); //=> returns title
+  
+  const finalArray = []
+  for (let i = 0; i < filmOneMatches.length; i++) {
+    for (let j = 0; j < filmTwoMatches.length; j++) {
+      if (filmOneMatches[i].id === filmTwoMatches[j].id) {
+        finalArray.push([filmOneMatches[i], filmTwoMatches[j]])
+      }
     }
-    setCategoryList(list)
-  }, []);
+  }
+  console.log(resultOne.base.name.split(' ')[0]);
+  
+  // console.log('FINAL ARRAY', finalArray);
+  // console.log(finalArray[0][0].title); // => returns title
+  // const test = finalArray.map((data) => data.map((nested) => nested.title)) // => returns title
+  // console.log(test);
 
   return (
-    <section>
-      {result.result.base && (
-        <>
-          <div>{result.result.base.name}</div>
-          <div>
-            {result.result.filmography.map((data) => {
-              return (
-                <div key={uuidv4()}>
-                  <span>{data.title}</span> -&nbsp;
-                  <span className='capitalize'>
-                    {data.job || data.category}
-                  </span>
-                  <br />
-                </div>
-              );
-            })}
+    <main>
+      <p className='text-center'>It seems like {resultOne.base.name} and {resultTwo.base.name} may have crossed paths in these productions:</p>
+      {finalArray.map((outer) => {
+        return (
+          <div className='border-y py-2'>
+            <p className='text-xl font-bold text-center'>{outer[0].title}</p>
+            <p className='text-center'>{resultOne.base.name.split(' ')[0]} worked as {outer[0].job || outer[0].category}</p>
+            <p className='text-center'>{resultTwo.base.name.split(' ')[0]} as {outer[1].job || outer[0].category}</p>
           </div>
-        </>
-      )}
-    </section>
+        )
+      })}
+    </main>
   );
 }
 
 export default Results;
 
-  // for (let i = 0; i < filmArray.length; i++) {
-  //   if (categoryList.indexOf(filmArray[i].category) === -1) {
-  //     categoryList.push(filmArray[i].category);
-  //   }
-  // }
+// Works
+// const uniqueEntries = Array.from(new Set(filmArray.map(a => a.id))).map(id => filmArray.find(a => a.id === id));
+// console.log(uniqueEntries);
 
-  // Works
-  // const uniqueEntries = Array.from(new Set(filmArray.map(a => a.id))).map(id => filmArray.find(a => a.id === id));
-  // console.log(uniqueEntries);
+// <section>
+//   {result.result.base && (
+//     <>
+//       <div>{result.result.base.name}</div>
+//       <div>
+//         {result.result.filmography.map((data) => {
+//           return (
+//             <div key={uuidv4()}>
+//               <span>{data.title}</span> -&nbsp;
+//               <span className='capitalize'>
+//                 {data.job || data.category}
+//               </span>
+//               <br />
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </>
+//   )}
+// </section>
+
+  //   const filmArray = resultOne.result.filmography;
+  //   const list = {};
+  //   for (const index of filmArray) {
+  //     list[index.category] = list[index.category] ? list[index.category] + 1 : 1;
+  //   }
+  //   setCategoryList(list)
+
+
+  /*
+  const idArrayOne = resultOne.filmography.map(a => a.id);
+  const idArrayTwo = resultTwo.filmography
+
+  const badCategories = ['archive_footage', 'self', 'thanks', 'soundtrack']
+  const test = idArrayTwo.filter(({ id }) => idArrayOne.includes(id)).filter(({ category }) => !badCategories.includes(category)).map(a => a.id);
+  */
